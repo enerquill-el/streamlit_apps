6638d95bc461d8f8531ffb574c67053d3f587667
enerquill-el <elaineyl.iuliano@gmail.com>
2025-08-23 09:42:36 +0200

    Update MC_App1.py

--- mc_app_base/MC_App1.py
+++ mc_app_base/MC_App1.py
@@ -766,11 +766,11 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 450px;
+        width: 400px;
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 450px;
-        margin-left: -450px;
+        width: 400px;
+        margin-left: -400px;
     }
     </style>
     """,
